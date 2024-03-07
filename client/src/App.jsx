525afe6bde81c4e5fea5175dc33b3190d5a3e121
mehul1409 <mehulbansalswm1234@gmail.com>
2024-03-07 18:45:43 +0530

    Team member

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,24 +3,23 @@ import Footer from './Components/Footer/Footer.jsx';
 import Layout from './pages/Layout.jsx';
 import Register from './pages/Register/Register.jsx';
 import Team from './pages/Team/Team.jsx';
-import { BrowserRouter , Routes , Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
       <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Layout/>} />
-        <Route path='/register' element={<Register/>} />
-        <Route path='/team' element={<Team />} />
-      </Routes>
-      <Footer />
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Layout />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/team' element={<Team />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
 }
 
 export default App;
-  
